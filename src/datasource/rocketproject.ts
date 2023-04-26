c41c283c48324e2f2f3419196d028f30a502692f
marinadiniz-plank <marina.diniz@joinplank.com>
2023-04-26 15:44:48 -0300

    updating datasource

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -7,7 +7,7 @@ import { Crewman } from "../models/Crewman";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.TYPEORM_HOST || "kashin.db.elephantsql.com",
-  port: 5432,
+  port: 80,
   username: process.env.TYPEORM_USERNAME || "auvjtyxo",
   password: process.env.TYPEORM_PASSWORD || "CEexuQTFOLe3-JoZgLXn-rardzeVI6Qi",
   database: process.env.TYPEORM_DATABASE || "auvjtyxo",
