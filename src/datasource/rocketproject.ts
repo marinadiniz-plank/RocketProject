0332cb95766dad8fedef00c05fd20d59ab621615
marinadiniz-plank <marina.diniz@joinplank.com>
2023-04-27 14:12:15 -0300

    removing white spaces

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.TYPEORM_HOST ,
+  host: process.env.TYPEORM_HOST,
   port: 5432,
-  username: process.env.TYPEORM_USERNAME ,
-  password: process.env.TYPEORM_PASSWORD ,
-  database: process.env.TYPEORM_DATABASE ,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
   entities: ["src/models/*.ts"],
   migrations: ["src/database/migrations/*.ts"],
   synchronize: true,
