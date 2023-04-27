0648d4fb18d894d4e619ee2a4e96ec1d724315ba
marinadiniz-plank <marina.diniz@joinplank.com>
2023-04-27 13:42:44 -0300

    updating port

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.TYPEORM_HOST || "kashin.db.elephantsql.com",
-  port: 80,
+  port: 5432,
   username: process.env.TYPEORM_USERNAME || "auvjtyxo",
   password: process.env.TYPEORM_PASSWORD || "CEexuQTFOLe3-JoZgLXn-rardzeVI6Qi",
   database: process.env.TYPEORM_DATABASE || "auvjtyxo",
