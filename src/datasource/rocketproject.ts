1f9b60605ea9826edab206fffd3b3f5a71eb7c2a
marinadiniz-plank <marina.diniz@joinplank.com>
2023-04-27 14:08:58 -0300

    one more time

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.TYPEORM_HOST || "kashin.db.elephantsql.com",
+  host: process.env.TYPEORM_HOST ,
   port: 5432,
-  username: process.env.TYPEORM_USERNAME || "auvjtyxo",
-  password: process.env.TYPEORM_PASSWORD || "CEexuQTFOLe3-JoZgLXn-rardzeVI6Qi",
-  database: process.env.TYPEORM_DATABASE || "auvjtyxo",
+  username: process.env.TYPEORM_USERNAME ,
+  password: process.env.TYPEORM_PASSWORD ,
+  database: process.env.TYPEORM_DATABASE ,
   entities: ["src/models/*.ts"],
   migrations: ["src/database/migrations/*.ts"],
   synchronize: true,
