3d920dbcba7f56167d00ae9ad13ed201a0c4c2bb
marinadiniz-plank <marina.diniz@joinplank.com>
2023-04-27 14:27:08 -0300

    updating paths

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -7,8 +7,8 @@ export const AppDataSource = new DataSource({
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: ["src/models/*.{ts,js}"],
-  migrations: ["src/database/migrations/*.{ts,js}"],
+  entities: ['./**/models/*.{ts,js}'],
+  migrations: ['./**/database/migrations/*.{ts,js}'],
   synchronize: true,
   logging: false,
 }); 
