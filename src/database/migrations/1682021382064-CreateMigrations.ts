956625644d793b03f492e197a63be0e9e9f7e3a0
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-22 09:47:12 -0300

    updating migrations

--- src/database/migrations/1682021382064-CreateMigrations.ts
+++ src/database/migrations/1682021382064-CreateMigrations.ts
@@ -4,9 +4,9 @@ export class CreateMigrations1682021382064 implements MigrationInterface {
     name = 'CreateMigrations1682021382064'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "crewman" ("name" character varying NOT NULL, "id" SERIAL NOT NULL, "patent" character varying NOT NULL, CONSTRAINT "PK_4fb6d9687691fd6b62b98a62955" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "rocket" ("name" character varying NOT NULL, "id" SERIAL NOT NULL, CONSTRAINT "PK_89b0efae402998623e1367aa34a" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "crew" ("name" character varying NOT NULL, "id" SERIAL NOT NULL, CONSTRAINT "PK_cc72b429996b3476dbaac59f1c2" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "crewman" ("id" SERIAL NOT NULL, "name" character varying NOT NULL,  "patent" character varying NOT NULL, CONSTRAINT "PK_4fb6d9687691fd6b62b98a62955" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "rocket" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_89b0efae402998623e1367aa34a" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "crew" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_cc72b429996b3476dbaac59f1c2" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "launch" ("id" SERIAL NOT NULL, "launchCode" integer NOT NULL, "date" TIMESTAMP NOT NULL DEFAULT now(), "success" boolean NOT NULL, "rocket_id" integer, "crew_id" integer, CONSTRAINT "REL_968817f025629bca18fe3f56bb" UNIQUE ("rocket_id"), CONSTRAINT "REL_3913e7efe32c3b5394fdcb7379" UNIQUE ("crew_id"), CONSTRAINT "PK_0efd83695074312cab129ff59f0" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "crew_crewman" ("crewId" integer NOT NULL, "crewmanId" integer NOT NULL, CONSTRAINT "PK_72f5b4c0b353f409d8088ee7835" PRIMARY KEY ("crewId", "crewmanId"))`);
         await queryRunner.query(`CREATE INDEX "IDX_1a360a675365254b4c18f44bf6" ON "crew_crewman" ("crewId") `);
