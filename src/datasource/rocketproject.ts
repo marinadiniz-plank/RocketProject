239a2fc10c593f3ed2a72ce288ae9f649e86162f
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-22 10:38:59 -0300

    updating migrations

--- src/datasource/rocketproject.ts
+++ src/datasource/rocketproject.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.TYPEORM_HOST,
+  host: process.env.TYPEORM_HOST || "localhost",
   port: parseInt(process.env.TYPEORM_PORT || "5432"),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+  username: process.env.TYPEORM_USERNAME || "postgres",
+  password: process.env.TYPEORM_PASSWORD|| "postgres",
+  database: process.env.TYPEORM_DATABASE|| "rocket_project",
   entities: ['./**/models/*.{ts,js}'],
   migrations: ['./**/database/migrations/*.{ts,js}'],
   synchronize: true,
