65814095b2deb80133f02fc421b4dbab3d9f7671
marinadiniz-plank <marina.diniz@joinplank.com>
2023-06-07 10:01:05 -0300

    updating cors

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,20 +1,24 @@
 import cors from "cors";
 
-const corsWhiteList = ['https://www.google.com', 'null', 'http://127.0.0.1:5500', 'http://localhost:5173', 'https://rocketproject-frontend.vercel.app/', 'https://rocketproject-frontend-c15cr9767-marinadiniz-plank.vercel.app/'];
+const corsWhiteList = [
+  "https://www.google.com",
+  "null",
+  "http://127.0.0.1:5500",
+  "http://localhost:5173",
+  "https://rocketproject-frontend.vercel.app",
+];
 
 const corsOption: cors.CorsOptions = {
-	origin: (origin, callback) => {
-		if (origin == undefined || corsWhiteList.indexOf(origin) !== -1) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'))
-		}
-	},
-	optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    if (origin == undefined || corsWhiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
 
 const CorsConfig = cors(corsOption);
 
-export {
-	CorsConfig
-};
+export { CorsConfig };
